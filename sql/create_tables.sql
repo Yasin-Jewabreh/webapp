f6df0c7900b8f78635e7b272804365bb7b1fdee9
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-17 20:21:20 +0200

    add table "nutzer" and edit table "auftraege"

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,12 +1,26 @@
-DROP TABLE IF EXISTS auftraege;
+
+CREATE TABLE nutzer (
+    nutzer_id Integer PRIMARY KEY AUTOINCREMENT,
+    vorname TEXT NOT NULL,
+    nachname TEXT NOT NULL, 
+    geburtsdatum TEXT NOT NULL,
+    straße TEXT NOT NULL,
+    hausnummer Integer NOT NULL,
+    postleitzahl Integer NOT NULL,
+    ort TEXT NOT NULL,
+    helfer BOOLEAN NOT NULL
+);
+
 
 CREATE TABLE auftraege (
-    id Integer PRIMARY KEY AUTOINCREMENT
+    auftrags_id Integer PRIMARY KEY AUTOINCREMENT,
+    nutzer_id Integer,
+    vorname TEXT,
     hilfsart TEXT NOT NULL,
-    datum TEXT NOT NULL,
-    uhrzeit TEXT NOT NULL,
-    dauer REAL NOT NULL,
     adresse TEXT NOT NULL,
     beschreibung TEXT NOT NULL,
-    status TEXT NOT NULL DEFAULT 'offen'
+    status TEXT NOT NULL DEFAULT 'offen',
+    FOREIGN KEY (nutzer_id) REFERENCES nutzer (nutzer_id)
+    FOREIGN KEY (vorname) REFERENCES nutzer (vorname)
 );
+
