7a3aeb5e0391214eefe84d0309a49f5a83c90ccc
yvzslm2903 <s_tiryaki24@stud.hwr-berlin.de>
2026-06-17 16:33:25 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import database
+from flask import Flask, render_template, redirect, url_for, request
+import os 
+import database
+
+app = Flask(__name__)
+
+os.makedirs(app.instance_path, exist_ok=True)
+
+app.config.from_mapping(
+    DATABASE = os.path.join(app.instance_path, "Pflegehilfe.db")
+    )
+
+database.init_app(app)
+
+@app.route("/")
+def startseite():
+    return "Die Startseite funktioniert"
+
+@app.route("/auftraege")
+def auftraege():
+    return "Auftragsübersicht funktioniert"
+
+@app.route("/db-test")
+def db_test():
+    db_con = database.get_db_con()
+    result =db_con.execute("Select 1").fetchone() #ohne fetchone würde es nur drauf zeigen: <sqlite3.Cursor object at 0x...>
+
+    return f"Datenbank funktioniert: {result[0]}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
