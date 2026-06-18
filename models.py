c1d9e90ce7b4a0f868bbbf93fbcac1a5160916b9
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-18 16:07:07 +0200

    make first letter of class name big

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from db import db
 
-class nutzer(db.Model):
+class Nutzer(db.Model):
     __tablename__ = "nutzer"
 
     id = db.Column(db.Integer, primary_key = True, index = True)
@@ -15,31 +15,31 @@ class nutzer(db.Model):
     passwort = db.Column(db.String(), nullable = False)
     rolle = db.Column(db.String(), nullable = False, index = True)
 
-class auftrag(db.Model):
+class Auftrag(db.Model):
     __tablename__ = "auftrag"
 
     id = db.Column(db.Integer, primary_key = True, index = True)
     wohnsituation = db.Column(db.String(100), nullable = False)
     beschreibung = db.Column(db.String(500), nullable = False)
     status = db.Column(db.String(50), default = "offen", nullable = False)
-    nutzer_id = db.Column("nutzer_id", db.ForeignKey("nutzer.id"), nullable = False)
+    nutzer_id = db.Column("nutzer_id", db.ForeignKey("Nutzer.id"), nullable = False)
     
     
-class nachricht (db.Model):
+class Nachricht (db.Model):
     __tablename__ = "nachricht"
 
     id = db.Column(db.Integer, primary_key = True)
     inhalt = db.Column(db.String(500), nullable=False)
     zeitstempel = db.Column(db.DateTime, default=db.func.now())
-    sender_id=db.Column("sender_id", db.ForeignKey("nutzer.id"), nullable = False)
-    empfaenger_id= db.Column('empfaenger_id', db.ForeignKey("nutzer.id"), nullable = False)
+    sender_id=db.Column("sender_id", db.ForeignKey("Nutzer.id"), nullable = False)
+    empfaenger_id= db.Column('empfaenger_id', db.ForeignKey("Nutzer.id"), nullable = False)
  
-class termin (db.Model):
+class Termin (db.Model):
     __tablename__ = "termin"
 
     id = db.Column(db.Integer, primary_key = True, index = True)
-    helfer_id = db.Column("helfer_id", db.ForeignKey("nutzer.id"), nullable = False)
-    pp_id = db.Column("pp_id", db.ForeignKey("nutzer.id"), nullable = False)
+    helfer_id = db.Column("helfer_id", db.ForeignKey("Nutzer.id"), nullable = False)
+    pp_id = db.Column("pp_id", db.ForeignKey("Nutzer.id"), nullable = False)
     titel = db.Column(db.String(), nullable = False)
     notizen = db.Column(db.String(200), nullable = True)
     datum = db.Column(db.Date, nullable = False)
