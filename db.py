b78f8a85f5eaabf25d984072ccb26478aedf3c1b
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-18 14:59:25 +0200

    edit db-py to fit with models.py

--- db.py
+++ db.py
@@ -1,9 +1,2 @@
-import click
 from flask_sqlalchemy import SQLAlchemy  # (1.)
-from sqlalchemy import orm
-from app import app
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Pflegehilfe.db'  # (2.)
-
 db = SQLAlchemy()  # (3.)
-db.init_app(app)  # (4.)
