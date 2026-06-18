e93dd41d59a7be2bc004156da63d390a4107e67c
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-18 12:29:18 +0200

    create table termin

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -11,16 +11,25 @@ CREATE TABLE nutzer (
     helfer BOOLEAN NOT NULL
 );
 
-
-CREATE TABLE auftraege (
+CREATE TABLE auftrag (
     auftrags_id Integer PRIMARY KEY AUTOINCREMENT,
-    nutzer_id Integer,
+    nutzer_id Integer PRIMARY KEY,
     vorname TEXT,
-    hilfsart TEXT NOT NULL,
     adresse TEXT NOT NULL,
     beschreibung TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'offen',
     FOREIGN KEY (nutzer_id) REFERENCES nutzer (nutzer_id)
     FOREIGN KEY (vorname) REFERENCES nutzer (vorname)
+);
+
+CREATE TABLE termin (
+    auftrags_id Integer,
+    nutzer_id_helfer Integer PRIMARY KEY,
+    nutzer_id_pp Integer PRIMARY KEY,
+    datum TEXT NOT NULL,
+    uhrzeit_beginn TEXT NOT NULL,
+    uhrzeit_ende TEXT NOT NULL,
+    FOREIGN KEY (nutzer_id_helfer) REFERENCES nutzer (nutzer_id) ON DELETE CASCADE,
+    FOREIGN KEY (nutzer_id_pp) REFERENCES nutzer (nutzer_id) ON DELETE CASCADE
 );
 
