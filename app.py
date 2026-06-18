4be795597ec5a60eaf21d8f2ef3b21d1018b176f
yvzslm2903 <s_tiryaki24@stud.hwr-berlin.de>
2026-06-18 15:56:00 +0200

    Update der Auftrag erstellung in app.py

--- app.py
+++ app.py
@@ -17,8 +17,37 @@ def startseite():
     return "Die Startseite funktioniert"
 
 @app.route("/auftraege")
-def auftraege():
-    return "Auftragsübersicht funktioniert"
+@login_required
+def auftraege_start():
+    if current_user.rolle == "PP":
+        return redirect (url_for("auftrag_erstellen"))
+
+    if current_user.rolle == "Helfer":
+        return redirect (url_for("helfer_auftraege"))
+
+    return "Unbekante Benutzerrolle", 403
+
+@app.route("/auftrag/erstellen", methods =["GET", "POST"])
+@login_required
+def auftrag_erstellen():
+    if request.method == "POST":
+        neuer_auftrag = auftrag(
+            wohnsituation = request.form["wohnsituation"],
+            beschreibung = request.form["beschreibung"],
+            status = "offen",
+            nutzer_id = current_user.id
+        )
+
+        db.session.add(neuer_auftrag)
+        db.session.commit()
+
+        return redirect(url_for("auftraege_start"))
+    
+    return render_template(
+        "auftrag_erstellen.html",
+        nutzer = current_user
+    )
+
 
 @app.route("/termine")
 def kalender():
