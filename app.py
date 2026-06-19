8b2c329f198fbffbb49f7aaa2af3553b1bf513b2
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-19 16:35:51 +0200

    add sample data and form handling (not finished)

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from db import db
-from datetime import date, time
-from flask import Flask, render_template, redirect, url_for, request
+from datetime import date, time, datetime
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap5
-from models import Termin, Nutzer, Auftrag, Nachricht
 import forms
 
 app = Flask(__name__)
+
+from db import db
+from models import Termin, Nutzer, Auftrag, Nachricht
 
 app.config["SECRET_KEY"] = "secret_key_just_for_dev_environment"
 app.config["BOOTSTRAP_BOOTSWATCH_THEME"] = "pulse"
@@ -17,49 +18,55 @@ db.init_app(app)
 bootstrap = Bootstrap5(app)
 
 with app.app_context():
-    db.create_all()
-    if not Nutzer.query.first():
-        # 1. Einen Test-Nutzer (Helfer/Hilfesuchender) anlegen
-        test_nutzer = Nutzer(
-            vorname="Max", nachname="Mustermann", geschlecht="Männlich",
-            geburtsdatum=date(1990, 1, 1), adresse="Musterstraße 1",
-            plz="12345", ort="Musterstadt", email="max@example.com",
-            passwort="geheimspasswort", rolle="Helfer"
-        )
-        db.session.add(test_nutzer)
-        db.session.commit() # Speichern, um die ID für die ForeignKeys zu generieren
-        
-        # 2. Einen Test-Auftrag anlegen
-        test_auftrag = Auftrag(
-            wohnsituation="Einfamilienhaus",
-            beschreibung="Rasen mähen im Vorgarten",
-            angenommen = False,
-            nutzer_id=test_nutzer.id
-        )
-        db.session.add(test_auftrag)
-        db.session.commit()
-        
-        # 3. Zwei Test-Termine anlegen (einen offenen, einen erledigten)
-        termin1 = Termin(
-            helfer_id=test_nutzer.id, pp_id=test_nutzer.id, auftrag_id=test_auftrag.id, notizen="Rasenmäher steht im Schuppen",
-            datum=date(2026, 7, 20), uhrzeit_beginn=time(14, 0), uhrzeit_ende=time(16, 0),
-            complete=False
-        )
-        termin2 = Termin(
-            helfer_id=test_nutzer.id, pp_id=test_nutzer.id, auftrag_id=test_auftrag.id,
-            notizen="Einkaufszettel liegt auf dem Küchentisch",
-            datum=date(2026, 6, 15), uhrzeit_beginn=time(10, 0), uhrzeit_ende=time(11, 30),
-            complete=False
-        )
-        termin3 = Termin(
-            helfer_id=test_nutzer.id, pp_id=test_nutzer.id, auftrag_id=test_auftrag.id, notizen="Einkaufszettel liegt auf dem Küchentisch",
-            datum=date(2026, 6, 15), uhrzeit_beginn=time(12, 0), uhrzeit_ende=time(13, 30),
-            complete=False
-        )
+    db.session.execute(db.delete(Nutzer))
+    db.session.execute(db.delete(Auftrag))
+    db.session.execute(db.delete(Nachricht))
+    db.session.execute(db.delete(Termin))
 
-        db.session.add_all([termin1, termin2, termin3])
-        db.session.commit()
-        print("🎉 Testdaten erfolgreich angelegt!")
+    helfer1= Nutzer(vorname = "Yasin", nachname = "Jason", 
+                    geschlecht = "M", geburtsdatum = datetime.strptime("27.12.2008", "%d.%m.%Y").date(),
+                     adresse = "Bischofshasener Straße 102", plz ="235453",
+                      ort = "Berlin", email ="y.yasin@web.de",
+                       passwort = "12345", rolle = "Helfer")
+    
+    helfer2= Nutzer(vorname = "Ahmad", nachname = "Patron", 
+                    geschlecht = "M", geburtsdatum = datetime.strptime("25.08.2004", "%d.%m.%Y").date(),
+                     adresse = "Badensche Straße 8", plz ="10557",
+                      ort = "Berlin", email ="a.patron@gmail.com",
+                       passwort = "11111", rolle = "Helfer")
+    
+    pp1= Nutzer(vorname = "Alexander", nachname = "Eck", 
+                    geschlecht = "M", geburtsdatum = datetime.strptime("15.03.1986", "%d.%m.%Y").date(),
+                     adresse = "Heidestraße 5", plz ="12395",
+                      ort = "Berlin", email ="a.eck@gmail.com",
+                       passwort = "22222", rolle = "PP")
+    
+    pp2= Nutzer(vorname = "Mohamed", nachname = "Aoun", 
+                    geschlecht = "M", geburtsdatum = datetime.strptime("30.03.1999", "%d.%m.%Y").date(),
+                     adresse = "Irgendwo in Libanon", plz ="19999",
+                      ort = "Berlin", email ="m.aoun@gmail.com",
+                       passwort = "33333", rolle = "PP")
+    
+    db.session.add_all([helfer1, helfer2, pp1, pp2])
+    db.session.commit()
+    
+    auftrag1 = Auftrag(wohnsituation = "Alleinstehend", 
+                       beschreibung = "Spende bittööö",
+                       angenommen = True, helfer_id = helfer1.id, pp_id = pp1.id)
+    
+    db.session.add_all([auftrag1])
+    db.session.commit()
+    
+    termin1 = Termin(helfer_id = helfer1.id, auftrag_id = auftrag1.id, 
+                     pp_id = pp1.id, notizen = "Bring bitte Geld mit", 
+                     datum = datetime.strptime("23.06.2026", "%d.%m.%Y").date(),   
+                     uhrzeit_beginn = datetime.strptime("12:30", "%H:%M").time(),         
+                     uhrzeit_ende = datetime.strptime("14:30", "%H:%M").time(), complete = False)
+    
+    db.session.add_all( [termin1])
+    db.session.commit()
+
+
 
 @app.route("/")
 def startseite():
@@ -71,25 +78,26 @@ def auftraege():
 
 @app.route("/termine/", methods = ["GET", "POST"])
 def termine():
-    termin_liste=  db.session.execute(db.select(Termin).where(Termin.complete == False).order_by(Termin.datum, Termin.uhrzeit_beginn)).scalars().all()
-    
     form = forms.TerminErstellenForm()
-
-    if form.validate_on_submit():
-        neuer_termin = Termin(
-            datum = form.datum.data,
-            uhrzeit_beginn = form.uhrzeit_beginn.data,
-            uhrzeit_ende = form.uhrzeit_ende.data,
-            notizen = form.notizen.data,
-            complete = False,
-            auftrag_id = 1,
-            pp_id = 1,
-            helfer_id = 1
-        )
-        db.session.add(neuer_termin)
-        db.session.commit()
+    if request.method == "GET":
+        termin_liste=  db.session.execute(db.select(Termin).where(Termin.complete == False).order_by(Termin.datum, Termin.uhrzeit_beginn)).scalars().all()
+        return render_template("termine.html", termine = termin_liste, form = form)
+    else:
+        if form.validate():
+            termin = Termin(helfer_id = helfer1.id,
+                            auftrag_id = auftrag1.id,
+                            pp_id = pp1.id, 
+                            notizen = form.notizen.data, 
+                            datum=form.datum.data, 
+                            uhrzeit_beginn = form.uhrzeit_beginn.data, 
+                            uhrzeit_ende = form.uhrzeit_ende.data                                                          
+                            )
+            db.session.add(termin)
+            db.session.commit()
+            flash("Termin wurde eingetragen.", "success")
+        else:
+            flash("Ups, hat nicht geklappt", "warning")
         return redirect (url_for("termine"))
-    return render_template("termine.html",termine = termin_liste, form = form)
 
 
 
