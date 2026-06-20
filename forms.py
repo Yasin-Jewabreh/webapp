8d4c1744ef472d938ba9a8cb481ffe4399ff076a
Yasin Jewabreh <s_jewabreh24@stud.hwr-berlin.de>
2026-06-20 18:52:35 +0200

    add validation for date and time

--- forms.py
+++ forms.py
@@ -1,11 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField, DateField, TimeField
-from wtforms.validators import InputRequired, Length, Optional
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.fields import DateField, TimeField
+from wtforms.widgets import DateInput, TimeInput
+from wtforms.validators import InputRequired, Optional, ValidationError
+from datetime import date
 
+
+def check_datum(self, field):
+        if field.data:
+            if field.data.year < date.today().year or  field.data.year > date.today().year +1 :
+                raise ValidationError(f"Das Datum muss im Jahr {date.today().year} oder {date.today().year +1} liegen!")
+
+def check_uhrzeit(self, field):
+        if self.uhrzeit_beginn.data and field.data:
+            if field.data <= self.uhrzeit_beginn.data:
+                raise ValidationError("Die End-Uhrzeit muss nach der Start-Uhrzeit liegen!")
+            
 class TerminErstellenForm(FlaskForm):
-    teilnehmer = SelectField("Der Termin ist mit:", validators=[InputRequired()])
-    datum = DateField("Am",validators=[InputRequired()], format='%Y-%m-%d')
-    uhrzeit_beginn = TimeField("Von",validators=[InputRequired()])
-    uhrzeit_ende = TimeField("Bis",validators=[InputRequired()])
+    teilnehmer = SelectField("Der Termin ist mit:", coerce=int, validators=[InputRequired()])
+    datum = DateField("Am",validators=[InputRequired(), check_datum], format='%Y-%m-%d')
+    uhrzeit_beginn = TimeField("Von",validators=[InputRequired()],format='%H:%M')
+    uhrzeit_ende = TimeField("Bis",validators=[InputRequired(), check_uhrzeit], format='%H:%M')
     notizen = StringField("Trage hier deine Notizen ein",validators=[Optional()])
     eintragen = SubmitField("Termin eintragen")
