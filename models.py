2d5259f8b0094030d9a4ae2c728f785d05f2483d
yvzslm2903 <s_tiryaki24@stud.hwr-berlin.de>
2026-06-21 21:13:15 +0200

    UserMixin added for the login

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from db import db
+from flask_login import UserMixin
 
-class Nutzer(db.Model):
+
+
+class Nutzer(db.Model, UserMixin):
     __tablename__ = "nutzer"
 
     id = db.Column(db.Integer, primary_key = True, index = True)
