38b83a106dce6622f2eee6451089dd824338d843
yvzslm2903 <s_tiryaki24@stud.hwr-berlin.de>
2026-06-21 23:58:14 +0200

    Login implemented sample Data deleted (all working)

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 from datetime import date, datetime
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
-from models import db, Nutzer, Auftrag, Termin  # Termin mitimportiert
+from models import db, Nutzer, Auftrag, Termin  
 from forms import AuftragFormular
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-
+from flask import session 
 
 
 app = Flask(__name__)
@@ -22,7 +22,7 @@ login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Nutzer.query.get(int(user_id))
+    return db.session.get(Nutzer, int(user_id))
 
 db.init_app(app)
 bootstrap = Bootstrap5(app)
@@ -66,20 +66,26 @@ def login():
 
         if nutzer and nutzer.passwort == passwort:
             login_user(nutzer)
-            return redirect(url_for("auftrag_erstellen"))
+
+            if nutzer.rolle == "helfer":
+                return redirect(url_for("helfer_auftraege"))
+            else:
+                return redirect(url_for("auftrag_erstellen"))
 
     return render_template("login.html")
+
+
 
 @app.route("/logout")
 @login_required
 def logout():
-    logout_user()
-    return redirect(url_for("startseite"))
+    logout_user()  
+    session.clear() 
+    return redirect(url_for("login")) 
 
 @app.route("/auftrag/erstellen", methods=["GET", "POST"])
+@login_required
 def auftrag_erstellen():
-    
-    aktueller_nutzer = Nutzer.query.get(3)
 
     form = AuftragFormular()
 
@@ -88,7 +94,7 @@ def auftrag_erstellen():
         neuer_auftrag = Auftrag(
             wohnsituation=form.wohnsituation.data,  
             beschreibung=form.beschreibung.data,
-            nutzer_id=aktueller_nutzer.id if aktueller_nutzer else 3
+            nutzer_id=current_user.id 
         )
 
         db.session.add(neuer_auftrag)
@@ -99,13 +105,16 @@ def auftrag_erstellen():
     
 
 
-    return render_template("auftrag_erstellen.html", nutzer=aktueller_nutzer, form =form)
+    return render_template("auftrag_erstellen.html", nutzer=current_user, form =form)
 
 @app.route("/helfer/auftraege")
+@login_required
 def helfer_auftraege():
     
-    offene_auftraege = Auftrag.query.filter_by(angenommen="offen").all()
+    if current_user.rolle != "helfer":
+        return "Zugriff verweigert. Nur Helfer können diese Seite sehen.", 403
 
+    offene_auftraege = Auftrag.query.filter_by(angenommen="offen").all()
     heute = date.today()
 
     return render_template(
@@ -114,60 +123,28 @@ def helfer_auftraege():
         heute=heute
     )
 
+
 @app.route("/helfer/auftrag/<int:auftrag_id>")
+@login_required
 def auftrag_annehmen(auftrag_id):
     
-    auftrag = Auftrag.query.get_or_404(auftrag_id)
     
+    if current_user.rolle != "helfer":
+        return "Zugriff verweigert. Nur Helfer können diese Seite sehen.", 403
   
+    auftrag = db.session.get(Auftrag, auftrag_id)
+    if not auftrag:
+        return "Auftrag nicht gefunden", 404
+    
     auftrag.angenommen = "angenommen"
     db.session.commit()
     
     return render_template("auftrag_angenommen.html", auftrag=auftrag)
 
-
 if __name__ == "__main__":
     with app.app_context():
         
         db.create_all()
-        
-        
-        if Nutzer.query.get(3) is None:
-            
-            helfer1 = Nutzer(vorname="Yasin", nachname="Jason", geschlecht="M", 
-                             geburtsdatum=datetime.strptime("27.12.2008", "%d.%m.%Y").date(),
-                             adresse="Bischofshasener Straße 102", plz="235453", ort="Berlin", 
-                             email="y.yasin@web.de", telefon="0151111111", passwort="12345", rolle="Helfer")
-        
-            helfer2 = Nutzer(vorname="Ahmad", nachname="Patron", geschlecht="M", 
-                             geburtsdatum=datetime.strptime("25.08.2004", "%d.%m.%Y").date(),
-                             adresse="Badensche Straße 8", plz="10557", ort="Berlin", 
-                             email="a.patron@gmail.com", telefon="0151222222", passwort="11111", rolle="Helfer")
                 
-            pp1 = Nutzer(vorname="Alexander", nachname="Eck", geschlecht="M", 
-                         geburtsdatum=datetime.strptime("15.03.1986", "%d.%m.%Y").date(),
-                         adresse="Heidestraße 5", plz="12395", ort="Berlin", 
-                         email="a.eck@gmail.com", telefon="0151333333", passwort="22222", rolle="PP")
-                
-            pp2 = Nutzer(vorname="Mohamed", nachname="Aoun", geschlecht="M", 
-                         geburtsdatum=datetime.strptime("30.03.1999", "%d.%m.%Y").date(),
-                         adresse="Irgendwo in Libanon", plz="19999", ort="Berlin", 
-                         email="m.aoun@gmail.com", telefon="0151444444", passwort="33333", rolle="PP")
-            
-            db.session.add_all([helfer1, helfer2, pp1, pp2])
-            db.session.commit()
-                
-           
-           # auftrag1 = Auftrag(wohnsituation="Alleinstehend", beschreibung="Spende bittööö",
-            #                   angenommen="offen", nutzer_id=pp1.id)
-                
-            #db.session.add(auftrag1)
-            #db.session.commit()
-                
-        
-            print("Testdaten erfolgreich hinzugefügt!")
-        else:
-            print("Datenbank enthält bereits Daten. Testdaten-Generierung übersprungen.")
-
 
     app.run(debug=True)
