1ce3410c46e9b0afdf871a4733a20dabdaa0729d
SalihAbi2836 <s_oeztuerk24@stud.hwr-berlin.de>
2026-06-23 14:36:11 +0200

    feat: add WTForms classes for login, registration and profile

--- forms.py
+++ forms.py
@@ -1,7 +1,44 @@
+from datetime import date
 from flask_wtf import FlaskForm
-from wtforms import SelectField, TextAreaField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms import (StringField, PasswordField, DateField, HiddenField, SubmitField, SelectField, TextAreaField, BooleanField, EmailField)
+from wtforms.validators import (DataRequired, Email, EqualTo, Length, InputRequired, ValidationError)
 
+class RollenWahlForm(FlaskForm):
+    helfer_btn = SubmitField('Hilfe anbieten')
+    suchender_btn = SubmitField('Hilfe suchen')
+
+class RegisterForm(FlaskForm):
+
+    vorname = StringField('Vorname', validators=[DataRequired(message="Bitte Vornamen eingeben.")])
+    nachname = StringField('Nachname', validators=[DataRequired(message="Bitte Nachnamen eingeben.")])
+    geschlecht = SelectField("Geschlecht",choices=[('männlich', 'Männlich'), ('weiblich', 'Weiblich'), ('divers', 'Divers')],validators=[DataRequired(message="Bitte Geschlecht eingeben.")])
+    geburtsdatum = DateField('Geburtsdatum', format='%Y-%m-%d', validators=[DataRequired(message="Bitte Geburtsdatum auswählen.")])
+    adresse = StringField('Adresse', validators=[DataRequired(message="Bitte Adresse eingeben.")])
+    plz = StringField('PLZ', validators=[DataRequired(message="Bitte PLZ eingeben."), Length(min=5, max=5, message="PLZ muss 5-stellig sein.")])
+    ort = StringField("Ort", validators=[DataRequired(message="Bitte Ort eingeben.")])
+    email = StringField('E-Mail', validators=[DataRequired(message="Bitte E-Mail eingeben."), Email(message="Ungültige E-Mail-Adresse.")])
+    telefonnummer = StringField('Telefonnummer', validators=[DataRequired(message="Bitte Telefonnummer eingeben.")])
+    
+    passwort = PasswordField('Passwort', validators=[
+        DataRequired(message="Bitte Passwort eingeben."),
+        Length(min=6, message="Das Passwort muss mindestens 6 Zeichen lang sein.")
+    ])
+    passwort2 = PasswordField('Passwort bestätigen', validators=[
+        DataRequired(message="Bitte Passwort bestätigen."),
+        EqualTo('passwort', message='Passwörter müssen übereinstimmen.')
+    ])
+    
+    rolle = HiddenField('Rolle')
+    submit = SubmitField('Registrieren')
+class LoginForm(FlaskForm):
+    email = StringField('E-Mail', validators=[
+        DataRequired(message="Bitte geben Sie Ihre E-Mail-Adresse ein."),
+        Email(message="Ungültige E-Mail-Adresse.")
+    ])
+    passwort = PasswordField('Passwort', validators=[
+        DataRequired(message="Bitte geben Sie Ihr Passwort ein.")
+    ])
+    submit = SubmitField('Login')    
 class AuftragFormular(FlaskForm):
     wohnsituation = SelectField(
         "Wohnsituation",
@@ -16,7 +53,7 @@ class AuftragFormular(FlaskForm):
         ],
         validators=[DataRequired(message="Bitte wähle deine Wohnsituation aus.")]
     )
-    
+
     beschreibung = TextAreaField(
         "Über mich",
         validators=[
@@ -31,3 +68,20 @@ class AuftragFormular(FlaskForm):
     )
     
     submit = SubmitField("Auftrag veröffentlichen")
+
+def check_geburtsdatum(form, field):
+    if field.data:
+        if ((date.today() - field.data).days / 365.25) < 18:
+            raise ValidationError(
+                "Du musst mindestens 18 Jahre alt sein."
+            )
+
+class ProfilFormular(FlaskForm):
+    vorname = StringField('Vorname', validators=[DataRequired(message="Bitte Vornamen eingeben.")])
+    nachname = StringField('Nachname', validators=[DataRequired(message="Bitte Nachnamen eingeben.")])
+    email = StringField('E-Mail', validators=[DataRequired(message="Bitte E-Mail eingeben."), Email(message="Ungültige E-Mail.")])
+    telefon = StringField('Telefonnummer', validators=[DataRequired(message="Bitte Telefonnummer eingeben.")])
+    adresse = StringField('Adresse', validators=[DataRequired(message="Bitte Adresse eingeben.")])
+    plz = StringField('PLZ', validators=[DataRequired(message="Bitte PLZ eingeben.")])
+    ort = StringField('Ort', validators=[DataRequired(message="Bitte Ort eingeben.")])
+    submit = SubmitField('Änderungen speichern')
