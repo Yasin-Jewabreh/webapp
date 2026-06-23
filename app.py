dc102e740e9f50daf57a32eee2c12a693162d88c
yvzslm2903 <s_tiryaki24@stud.hwr-berlin.de>
2026-06-23 09:25:10 +0200

    dont delte the db table every time

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def chat(empfaenger_id):
 
 if __name__ == "__main__":
    with app.app_context():
-        db.drop_all()    
+        #db.drop_all()    
         db.create_all()  
     
         app.run(debug=True)
